f66df141848103973def08a0cb42a12dcee5912e
jangjichang <wlckd90@gmail.com>
2019-05-18 22:08:29 +0900

    추가 - Work, Card create, update에 사용되는 form 생성

---
+++ src/todolist/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.forms import ModelForm
+from django.forms.models import inlineformset_factory
+from .models import Work, Card
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class WorkPriorityInput(forms.Select):
+    choices = Work.CARD_PRIORITY
+
+
+class CardPriorityInput(forms.Select):
+    choices = Card.CARD_PRIORITY
+
+
+class CardForm(ModelForm):
+    class Meta:
+        model = Card
+        fields = ['name', 'description', 'deadline', 'priority', 'done']
+        widgets = {
+            'deadline': DateInput(),
+            'priority': CardPriorityInput(),
+        }
+
+
+CardInlineFormSet = inlineformset_factory(
+                        Work,
+                        Card,
+                        form=CardForm,
+                        can_delete=False,
+                        extra=1)
+
+
+class WorkForm(ModelForm):
+    class Meta:
+        model = Work
+        fields = ['name', 'description', 'deadline', 'priority', 'done']
+        widgets = {
+            'deadline': DateInput(),
+            'priority': WorkPriorityInput(),
+        }
