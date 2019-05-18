c2cc40e9d59f012b26840e5b82075dc6089c0ab0
jangjichang <wlckd90@gmail.com>
2019-05-18 22:20:10 +0900

    삭제 - package import 수정 비슷한 기능끼리 묶고 알파벳 순으로 정렬함

--- src/todolist/views.py
+++ src/todolist/views.py
@@ -1,10 +1,7 @@
 from django.http import Http404
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
-
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
-from .forms import CardForm, WorkForm
 
 from summercoding.views import LoginRequiredMixin
 from .models import Work, Card
