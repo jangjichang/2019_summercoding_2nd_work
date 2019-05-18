1772c3b95d60914b5199e3b999428ec96df9340a
jangjichang <wlckd90@gmail.com>
2019-05-18 19:04:52 +0900

    추가 - todolist app Card model 생성

--- src/todolist/models.py
+++ src/todolist/models.py
@@ -25,3 +25,26 @@ class Work(models.Model):
     def __str__(self):
         return self.name
 
+
+class Card(models.Model):
+    name = models.CharField(max_length=50, verbose_name="할 일 제목")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateField(auto_now_add=True)
+    modify_date = models.DateField(auto_now=True)
+    done = models.BooleanField(default=False, verbose_name="완료 시 선택해주세요")
+    description = models.CharField(max_length=50, blank=True, verbose_name="내용")
+    work = models.ForeignKey(Work, on_delete=models.CASCADE)
+    deadline = models.DateField(blank=True, null=True, verbose_name="마감 기한")
+
+    CARD_PRIORITY = (
+        ('1', '낮음'),
+        ('2', '중간'),
+        ('3', '높음'),
+    )
+    priority = models.CharField(max_length=1, choices=CARD_PRIORITY, default='1', verbose_name="우선 순위")
+
+    class Meta:
+        ordering = ['done', '-priority', 'deadline']
+
+    def __str__(self):
+        return self.name
