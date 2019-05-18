a59cdb0a12f6a4ad231298f80e91117a96678ea1
jangjichang <wlckd90@gmail.com>
2019-05-19 02:02:33 +0900

    수정 - 개발, 배포 환경에서 사용하는 코드 일치 시킴

--- src/summercoding/settings.py
+++ src/summercoding/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+try:
+    """
+    개발 환경이 아닌 경우, 배포 환경으로 셋팅함
+    """
+    if os.environ.get("summercoding_now_env") == 'dev':
+        from .dev_settings import *
+    else:
+        from .deploy_settings import *
+except ImportError as e:
+    print(e)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +30,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^c!zye9g=&wg2sq#2h4*vvzl6h&lftnrwux2v5jj&oyyb(@kr8'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = allowed_host
 
 
 # Application definition
