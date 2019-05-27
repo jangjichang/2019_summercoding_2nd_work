6b325913226a35721bf15115b5c4c1687f14b3f8
jangjichang <wlckd90@gmail.com>
2019-05-27 09:31:44 +0900

    추가 - admin page customize
    admin page 변경함 1. 완료 유무를 boolean field로 설정 2. Work field에 inline으로 Card 출력 3. name field를 기준으로 검색 기능 추가 4. create_data field를 기준으로 filter 추가

--- src/todolist/admin.py
+++ src/todolist/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Work, Card
 
+
+class CardInline(admin.TabularInline):
+    model = Card
+    extra = 0
+
+
+class WorkAdmin(admin.ModelAdmin):
+    inlines = [CardInline]
+    search_fields = ['name']
+    list_filter = ['create_date']
+    list_display = ('name', 'owner', 'create_date', 'modify_date', 'done')
+
+
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_filter = ['create_date']
+    list_display = ('name', 'owner', 'create_date', 'modify_date', 'done')
+
+
 # Register your models here.
-admin.site.register(Work)
-admin.site.register(Card)
+admin.site.register(Work, WorkAdmin)
+admin.site.register(Card, CardAdmin)
